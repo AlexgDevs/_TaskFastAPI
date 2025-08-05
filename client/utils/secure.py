f7f0f62755593199817d12f70c2f0e872b0e39b7
AlexgDevs <asmetanin014@gmail.com>
2025-08-05 21:19:10 +0500

    feat: created config flask_login

---
+++ client/utils/secure.py
@@ -0,0 +1,49 @@
+from functools import wraps
+from flask import redirect, url_for
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    current_user)
+from sqlalchemy import select
+
+from .. import app
+from ..db import (
+    Session,
+    User)
+
+app.secret_key = '123_test_123'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class LoginUser(UserMixin):
+    def __init__(self, id, name, role):
+        self.id = id
+        self.name = name
+        self.role = role
+
+
+@login_manager.user_loader
+def load_user(user_id: int):
+    with Session() as session:
+        user = session.scalar(select(User).where(User.id == user_id))
+        if user:
+            return LoginUser(
+                id=user.id,
+                name=user.name,
+                role=user.role
+            )
+
+    return None
+
+
+def admin_requried(f):
+    @wraps(f)
+    def wrapper(*arg, **kwarg):
+        if not current_user.is_authenticated or \
+            not current_user.role == 'admin':
+            return redirect(url_for('main'))
+        return f(*arg, **kwarg)
+    return wrapper
+
+
